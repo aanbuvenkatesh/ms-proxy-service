931c7b46e1297ebf7e1d979e8afb1f8271887466
Anbuvenkatesh A <aanbuvenkatesh@temenos.com>
2020-11-20 20:52:32 +0530

    Unit Test cases for Proxy Service

--- src/test/java/io/anbu/proxyservice/ProxyServiceApplicationTests.java
+++ src/test/java/io/anbu/proxyservice/ProxyServiceApplicationTests.java
@@ -1,13 +1,61 @@
 package io.anbu.proxyservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.anbu.proxyservice.constants.HttpRequestType;
+import io.anbu.proxyservice.controller.ProxyController;
+import io.anbu.proxyservice.view.ProxyServiceView;
+
+/**
+ * Basic test cases for proxy service.
+ * 
+ * @author aanbuvenkatesh
+ */
 @SpringBootTest
 class ProxyServiceApplicationTests {
 
+	@Autowired
+	private ProxyController controller;
+
 	@Test
-	void contextLoads() {
+	void shouldSucceedForValidRequest() {
+		ProxyServiceView requestPayload = new ProxyServiceView();
+		requestPayload.setUrl("https://www.google.com");
+		requestPayload.setRequestType(HttpRequestType.GET);
+		ResponseEntity<?> response = controller.proxyService("TEST", requestPayload);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+	@Test
+	void shouldThrowExceptionForMissingRequestBody() {
+		ProxyServiceView requestPayload = new ProxyServiceView();
+		requestPayload.setUrl("https://www.google.com");
+		requestPayload.setRequestType(HttpRequestType.POST);
+		assertThrows(ResponseStatusException.class, () -> controller.proxyService("TEST", requestPayload));
+	}
+
+	@Test
+	void shouldThrowExceptionIncaseOfInvalidHost() {
+		ProxyServiceView requestPayload = new ProxyServiceView();
+		requestPayload.setUrl("https://www.google1.com");
+		requestPayload.setRequestType(HttpRequestType.POST);
+		assertThrows(ResponseStatusException.class, () -> controller.proxyService("TEST", requestPayload));
+	}
+
+	@Test
+	void shouldThrowExceptionForNonHttpsURL() {
+		ProxyServiceView requestPayload = new ProxyServiceView();
+		requestPayload.setUrl("http://www.google.com");
+		requestPayload.setRequestType(HttpRequestType.GET);
+		assertThrows(ResponseStatusException.class, () -> controller.proxyService("TEST", requestPayload));
 	}
 
 }
